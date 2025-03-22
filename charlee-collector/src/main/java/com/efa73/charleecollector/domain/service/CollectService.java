b63b7ddeaff504b4b2f2dafdfc0b5053b9dd4c22
택택 <ongsttt52@gmail.com>
2025-03-22 19:26:23 +0900

    feat: 주기정보, 이벤트정보 중복 저장 예외처리 추가
    -[] 주기정보가 데이터베이스에 저장은 되었지만 응답 전송이 실패한 경우를 위한 로직 추가
    -[] 주기정보, 이벤트정보가 저장될 때 차량번호(mdn)와 시간이 중복되면 예외를 발생시키고 HttpCode 200을 반환하도록 함

--- charlee-collector/src/main/java/com/efa73/charleecollector/domain/service/CollectService.java
+++ charlee-collector/src/main/java/com/efa73/charleecollector/domain/service/CollectService.java
@@ -6,6 +6,7 @@ import com.efa73.charleecollector.domain.repository.CycleDataRepository;
 import com.efa73.charleecollector.domain.repository.CycleInfoRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,15 +17,23 @@ public class CollectService {
     private final CycleDataRepository cycleDataRepository;
 
     public String collectCycle(CycleInfoEntity cycleInfo, List<CycleDataEntity> cycleDataList) {
-        var saved = cycleInfoRepository.save(cycleInfo);
-        cycleDataRepository.saveAll(cycleDataList);
+        try {
+            var saved = cycleInfoRepository.save(cycleInfo);
+            cycleDataRepository.saveAll(cycleDataList);
 
-        return saved.getMdn();
+            return saved.getMdn();
+        } catch (DataIntegrityViolationException e) {
+            return "중복저장됨";
+        }
     }
 
     public String collectEvent(CycleInfoEntity eventInfo) {
-        var saved = cycleInfoRepository.save(eventInfo);
+        try {
+            var saved = cycleInfoRepository.save(eventInfo);
 
-        return saved.getMdn();
+            return saved.getMdn();
+        } catch (DataIntegrityViolationException e) {
+            return "중복저장됨";
+        }
     }
 }
