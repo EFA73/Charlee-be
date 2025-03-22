ce08338920b22dff0abbe5b1e0df5fbbadd286a6
yyysolhhh <solbaram37@gmail.com>
2025-03-23 03:18:58 +0900

    feat: User를 from 메서드로 생성
    User
    - @Getter 추가
    - 기본생성자 Protected로 제한
    - id 생성 strategy 명시
    - from 메서드 생성
    - 필요한 생성자 private으로 설정

--- charlee-web/src/main/java/com/efa73/charleeweb/user/domain/User.java
+++ charlee-web/src/main/java/com/efa73/charleeweb/user/domain/User.java
@@ -6,13 +6,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User extends Common {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
@@ -30,5 +36,15 @@ public class User extends Common {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public static User from(String name, String email, String password, String phone, Role role) {
+        return new User(name, email, password, phone, role);
+    }
 
+    private User(String name, String email, String password, String phone, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.role = role;
+    }
 }
