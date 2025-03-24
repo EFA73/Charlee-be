c65c7be810a465501737ab1ee4a1d77c79968aee
yyysolhhh <solbaram37@gmail.com>
2025-03-24 15:58:37 +0900

    feat: CSRF, 기본 formLogin 비활성화
    SecurityConfig
    - csrf, formLogin disable

--- charlee-web/src/main/java/com/efa73/charleeweb/config/SecurityConfig.java
+++ charlee-web/src/main/java/com/efa73/charleeweb/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.efa73.charleeweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+        ;
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
