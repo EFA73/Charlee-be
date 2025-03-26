3f947dd2c82fadd9b3b37ebb9df7ac58d7b2e09d
yyysolhhh <solbaram37@gmail.com>
2025-03-26 15:34:46 +0900

    feat: findByEmail 메서드 등록
    UserRepository
    - findByEmail 메서드 추가

--- charlee-web/src/main/java/com/efa73/charleeweb/user/domain/repository/UserRepository.java
+++ charlee-web/src/main/java/com/efa73/charleeweb/user/domain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.efa73.charleeweb.user.domain.repository;
 
 import com.efa73.charleeweb.user.domain.entity.Role;
 import com.efa73.charleeweb.user.domain.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByRole(Role role);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
