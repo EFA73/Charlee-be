11482dfb27094efe164289274194dcee0f2d92ed
yyysolhhh <solbaram37@gmail.com>
2025-03-26 16:43:53 +0900

    feat: EmptyAccessTokenException 커스텀 예외 생성
    - access token 비었을때 발생하는 예외

---
+++ charlee-web/src/main/java/com/efa73/charleeweb/common/exception/EmptyAccessTokenException.java
@@ -0,0 +1,7 @@
+package com.efa73.charleeweb.common.exception;
+
+public class EmptyAccessTokenException extends RuntimeException {
+    public EmptyAccessTokenException() {
+        super();
+    }
+}
