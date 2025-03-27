a7357d6b59cec26ee75254f13522da6c8687cfd6
kimchijinju <hanbimkim@gmail.com>
2025-03-27 16:41:15 +0900

    fix: 공통 예외처리가 변경되어 빌드가 되지 않는 문제 해결

--- charlee-web/src/main/java/com/efa73/charleeweb/company/domain/service/CompanyService.java
+++ charlee-web/src/main/java/com/efa73/charleeweb/company/domain/service/CompanyService.java
@@ -1,10 +1,10 @@
 package com.efa73.charleeweb.company.domain.service;
 
-import com.efa73.charleeweb.common.exception.CustomErrorCode;
+import com.efa73.charleeweb.common.exception.CharleeException;
+import com.efa73.charleeweb.common.exception.CommonErrorCode;
 import com.efa73.charleeweb.company.domain.entity.Company;
 import com.efa73.charleeweb.company.domain.repository.CompanyRepository;
 import com.efa73.charleeweb.company.interfaces.dto.request.CompanyRequest;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,13 +22,13 @@ public class CompanyService {
     public Company getCompany(Long companyId) {
 
         return companyRepository.findById(companyId)
-                .orElseThrow(() -> new EntityNotFoundException(CustomErrorCode.ENTITY_NOT_FOUND.getMessage()));
+                .orElseThrow(() -> new CharleeException(CommonErrorCode.NOT_FOUND));
     }
 
     public Company updateCompany(CompanyRequest companyRequest, Long companyId) {
 
         var company = companyRepository.findById(companyId)
-                .orElseThrow(() -> new EntityNotFoundException(CustomErrorCode.ENTITY_NOT_FOUND.getMessage()));
+                .orElseThrow(() -> new CharleeException(CommonErrorCode.NOT_FOUND));
 
         company.updateEntity(companyRequest.name(), companyRequest.siteLink());
 
@@ -38,7 +38,7 @@ public class CompanyService {
     public void deleteCompany(Long companyId) {
 
         if (!companyRepository.existsById(companyId)) {
-            throw new EntityNotFoundException(CustomErrorCode.ENTITY_NOT_FOUND.getMessage());
+            throw new CharleeException(CommonErrorCode.NOT_FOUND);
         }
         companyRepository.deleteById(companyId);
     }
