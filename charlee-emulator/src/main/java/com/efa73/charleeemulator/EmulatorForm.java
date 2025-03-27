d6d96a9d79fbb97e3a06f4cea6f7023f927c5820
cyunlee <siyunkr@gmail.com>
2025-03-27 18:41:49 +0900

    feat: 애뮬레이터 기본 프론트 화면 구현

---
+++ charlee-emulator/src/main/java/com/efa73/charleeemulator/EmulatorForm.java
@@ -0,0 +1,12 @@
+package com.efa73.charleeemulator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmulatorForm {
+    private String vehicleNumber;
+    private String route;
+    private String startDate;
+}
