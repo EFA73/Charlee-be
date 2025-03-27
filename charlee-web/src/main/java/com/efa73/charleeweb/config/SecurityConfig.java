265f1c4205d0e636044dc7f42958e086251f4de5
yyysolhhh <solbaram37@gmail.com>
2025-03-27 16:55:59 +0900

    feat: SecurityConfig 클래스에 관련 빈 추가
    - JWT 토큰을 검증하는 JwtAuthenticationFilter 추가
    - JSON 형식의 로그인 요청을 처리하는 CustomAuthenticationFilter 추가
    - 로그인 성공 및 실패 핸들러 추가
    - AuthenticationManager 및 PasswordEncoder 빈 설정
    - /api/login 엔드포인트에 대한 접근 허용

--- charlee-web/src/main/java/com/efa73/charleeweb/config/SecurityConfig.java
+++ charlee-web/src/main/java/com/efa73/charleeweb/config/SecurityConfig.java
@@ -1,24 +1,87 @@
 package com.efa73.charleeweb.config;
 
+import com.efa73.charleeweb.user.domain.repository.UserRepository;
+import com.efa73.charleeweb.user.login.filter.CustomAuthenticationFilter;
+import com.efa73.charleeweb.user.login.handler.LoginFailureHandler;
+import com.efa73.charleeweb.user.login.handler.LoginSuccessHandler;
+import com.efa73.charleeweb.user.login.service.CustomUserDetailsService;
+import com.efa73.charleeweb.util.JwtAuthenticationFilter;
+import com.efa73.charleeweb.util.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final ObjectMapper objectMapper;
+    private final UserRepository userRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
+                .addFilterBefore(jwtAuthenticationProcessingFilter(),
+                        CustomAuthenticationFilter.class
+                )
         ;
         return http.build();
+    }
+
+    @Bean
+    public Filter jwtAuthenticationProcessingFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider, userRepository);
+    }
+
+    @Bean
+    public Filter customJsonUsernamePasswordAuthenticationFilter() {
+        CustomAuthenticationFilter customAuthenticationFilter
+                = new CustomAuthenticationFilter(objectMapper);
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        customAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
+        customAuthenticationFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return customAuthenticationFilter;
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler(objectMapper);
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler(jwtTokenProvider, userRepository, objectMapper);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(customUserDetailsService);
+        return new ProviderManager(provider);
     }
 
     @Bean
