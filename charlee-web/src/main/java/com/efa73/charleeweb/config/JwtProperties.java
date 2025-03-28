4cbbd6258ca756443f70bd06be8b6511fb01047d
yyysolhhh <solbaram37@gmail.com>
2025-03-28 14:04:14 +0900

    feat: JwtProperties에서 @Setter 제거
    - 모든 변수 final로 변경
    - @RequiredArgsConstructor 추가

--- charlee-web/src/main/java/com/efa73/charleeweb/config/JwtProperties.java
+++ charlee-web/src/main/java/com/efa73/charleeweb/config/JwtProperties.java
@@ -1,33 +1,28 @@
 package com.efa73.charleeweb.config;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Setter //TODO: setter 안쓰는 방법 찾기
 @Getter
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-    private String secretKey;
-    private AccessTokenProperties access;
-    private RefreshTokenProperties refresh;
+    private final String secretKey;
+    private final AccessTokenProperties access;
+    private final RefreshTokenProperties refresh;
 
-    @Setter
     @Getter
-    @NoArgsConstructor
+    @RequiredArgsConstructor
     public static class AccessTokenProperties {
-        private Long expiration;
-        private String header;
+        private final Long expiration;
+        private final String header;
     }
 
-    @Setter
     @Getter
-    @NoArgsConstructor
+    @RequiredArgsConstructor
     public static class RefreshTokenProperties {
-        private Long expiration;
-        private String header;
+        private final Long expiration;
+        private final String header;
     }
 }
