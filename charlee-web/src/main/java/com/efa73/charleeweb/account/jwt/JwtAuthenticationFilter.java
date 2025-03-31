8239e389c08d5badfd845a1f93a342a40204d2a3
yyysolhhh <solbaram37@gmail.com>
2025-03-31 16:29:40 +0900

    feat: handleInvalidTokenException에서 에러 응답 생성
    - 로그인 실패시 ResponseEntity에 담아서 응답

--- charlee-web/src/main/java/com/efa73/charleeweb/account/jwt/JwtAuthenticationFilter.java
+++ charlee-web/src/main/java/com/efa73/charleeweb/account/jwt/JwtAuthenticationFilter.java
@@ -2,22 +2,26 @@ package com.efa73.charleeweb.account.jwt;
 
 import com.efa73.charleeweb.account.domain.entity.Account;
 import com.efa73.charleeweb.account.domain.repository.AccountRepository;
+import com.efa73.charleeweb.common.dto.ExceptionResponse;
 import com.efa73.charleeweb.common.exception.CharleeException;
 import com.efa73.charleeweb.common.exception.CommonErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.SecureRandom;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -35,7 +39,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
     private final AccountRepository userRepository;
 
-    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper(); //TODO: 뭐지
+    private final ObjectMapper objectMapper;
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -54,14 +59,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 .orElse(null);
 
         try {
-            if (accessToken != null && jwtTokenProvider.isTokenValid(accessToken)) {
+            if (jwtTokenProvider.isTokenValid(accessToken)) {
                 checkAccessTokenAndAuthentication(request, response, filterChain);
-            }
-            if (accessToken == null) {
-                throw new CharleeException(CommonErrorCode.EMPTY_ACCESS_TOKEN);
+            } else {
+                throw new CharleeException(CommonErrorCode.INVALID_ACCESS_TOKEN);
             }
         } catch (CharleeException e) {
-            log.info("Token is invalid or expired");
             handleInvalidTokenException(response, e);
         }
     }
@@ -69,11 +72,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private void handleInvalidTokenException(HttpServletResponse response, CharleeException e)
             throws IOException {
 
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+        ResponseEntity<ExceptionResponse> errorResponse =
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body(ExceptionResponse.of(e.getMessage()));
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                "Invalid or expired token"); // TODO: invalid token 테스트 후 수정 필요
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        objectMapper.writeValue(
+                response.getWriter(),
+                errorResponse.getBody()
+        );
     }
 
     /**
@@ -81,7 +89,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
      */
     private void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response,
                                                    FilterChain filterChain) throws ServletException, IOException {
-        log.info("call checkAccessTokenAndAuthentication()");
         jwtTokenProvider.extractAccessToken(request)
                 .filter(jwtTokenProvider::isTokenValid)
                 .ifPresent(accessToken -> {
@@ -101,7 +108,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private void saveAuthentication(Account user) {
         String password = user.getPassword();
 
-        UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+        UserDetails userDetails = User.builder()
                 .username(user.getEmail())
                 .password(password)
                 .roles(user.getRole().name())
@@ -110,20 +117,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                 userDetails, null, authoritiesMapper.mapAuthorities(userDetails.getAuthorities()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-    }
-
-    /**
-     * Password 랜덤 생성
-     */
-    private String generateRandomPassword(int length) {
-        final String CHAR_SET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+[]{}|;:,.<>?";
-        SecureRandom secureRandom = new SecureRandom();
-        StringBuilder password = new StringBuilder(length);
-
-        for (int i = 0; i < length; i++) {
-            int index = secureRandom.nextInt(CHAR_SET.length());
-            password.append(CHAR_SET.charAt(index));
-        }
-        return password.toString();
     }
 }
